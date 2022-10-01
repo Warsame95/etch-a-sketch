24d4c80298c0ca9c96bb414e1bc7ea751f263766
Warsame <warsameahmed95@gmail.com>
2022-10-01 17:36:28 +0100

    Added hover functionality to colour grid-items blue

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const container = document.querySelector('.grid-container');
+let gridItem;
+
+function colourBlue(e){
+    e.target.style.background = 'blue';
+}
 
 for (let i = 1; i <= 16*16; i++){
-    const gridItem = document.createElement('div');
+    gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
+    gridItem.addEventListener('mouseover',colourBlue);
     gridItem.textContent = i.toString();
     container.append(gridItem);
 }
