796f0300983d1894c3f94eb345eaca216445f59a
Warsame <warsameahmed95@gmail.com>
2022-10-02 00:16:16 +0100

    Removed numbers from grid

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ for (let i = 1; i <= 16*16; i++){
     gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     gridItem.addEventListener('mouseover',colourBlue);
-    gridItem.textContent = i.toString();
+    
     container.append(gridItem);
 }
 
