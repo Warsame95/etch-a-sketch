01779869ad86df014edb33b30f53bed38b6c964c
Warsame <warsameahmed95@gmail.com>
2022-10-08 17:11:06 +0100

    Added ability to change grid size using range bar

--- script.js
+++ script.js
@@ -22,12 +22,33 @@ function erase(e){
     gridItem.addEventListener('mouseover',colourApply);
 }
 
+function changeGrid(e){
+    // remove grid-items
+    let child = container.lastElementChild;
+    while(child){
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+    // create new grid
+    let size = e.target.value;
+    container.style.gridTemplateColumns = "repeat("+size+",1fr)";
+    for (let i = 1; i <= size*size; i++){
+        gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.addEventListener('mouseover',colourApply);
+        container.append(gridItem);
+    }
+
+}
+
 for (let i = 1; i <= 16*16; i++){
     gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     gridItem.addEventListener('mouseover',colourApply);
     container.append(gridItem);
 }
+const gridSize = document.getElementById('grid-size');
+gridSize.addEventListener('change',changeGrid);
 colourSelector.addEventListener('change',setColour)
 const eraseButton = document.getElementById('erase');
 eraseButton.addEventListener('click',erase);
